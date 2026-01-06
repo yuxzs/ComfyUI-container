2eb27aaf4bc81832c22fb92acd2a56b9b9a31284
yuxs <yjvync9v@gmail.com>
2026-01-06 16:25:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,6 @@ pip install -r requirements.txt --break-system-packages
 
 # pip install -r manager_requirements.txt --break-system-packages
 
-exec python3 ./main.py --listen
+exec python3 ./main.py --listen --enable-manager
 
 # python main.py --enable-manager
